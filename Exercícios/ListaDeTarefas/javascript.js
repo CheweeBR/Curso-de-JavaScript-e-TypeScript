29ddb73a1f317c5eb5f23e716b3484cbec93c803
Chewee <tiagoeloy@alunos.utfpr.edu.br>
2023-10-18 15:25:30 -0300

    Finalizando exercício da Aula 67 - Lista de tarefas

--- Exercícios/ListaDeTarefas/javascript.js
+++ Exercícios/ListaDeTarefas/javascript.js
@@ -1,22 +1,63 @@
 const tarefa = {
   objetivo: "",
-  check: true,
 };
 
-const list = [];
+const inputTarefa = document.querySelector("#inputTarefa");
+let list = [];
+let contador=0;
 
 function Adicionar() {
   let task = tarefa;
-  task.objetivo = document.querySelector("#inputTarefa").value;
-  task.check = false;
-  list.push(task);
-  ApresentarTarefa();
+  try {
+    if(inputTarefa.value != "") {
+      task.objetivo = inputTarefa.value;
+      list.push(task.objetivo);
+      SalvarLocalStorage();
+      ApresentarTarefa();
+    }
+    else {
+      throw new Error;
+    }
+    
+  } catch(err) {
+    alert('A Tarefa não pode estar vazia!');
+  }
 }
 
 function ApresentarTarefa() {
   let listar = document.querySelector("#lista");
   var li = document.createElement("li");
-  li.innerText = document.querySelector("#inputTarefa").value;
+  var button = document.createElement("button");
+  button.innerText = "Checar";
+  button.classList.add("botaoChecar");
+  li.innerText = inputTarefa.value;
+  li.classList.add("liStyle");
   listar.appendChild(li);
-
+  li.appendChild(button);
+  Limpar();
 }
+
+function Limpar(){
+  inputTarefa.value = "";
+}
+
+function SalvarLocalStorage() {
+  const listJSON = JSON.stringify(list);
+  localStorage.setItem('lista',listJSON);
+}
+
+inputTarefa.addEventListener("keypress", function(e) {
+  if(e.keyCode === 13){
+    Adicionar();
+  }
+})
+
+document.addEventListener("click", (e) => {
+  const el = e.target;
+  if (el.classList.contains('botaoChecar')){
+    let li = el.parentElement
+    li.classList.add('liStyleDone');
+    el.style.display = 'none';
+    SalvarLocalStorage();
+  }
+})
