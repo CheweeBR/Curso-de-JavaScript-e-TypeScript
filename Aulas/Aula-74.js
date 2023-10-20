dde11becdb90493419d8a77e2ba5d96504d2ee85
Chewee <tiagoeloy@alunos.utfpr.edu.br>
2023-10-20 16:33:26 -0300

    Aula-74.js

---
+++ Aulas/Aula-74.js
@@ -0,0 +1,13 @@
+// IIFE -> Imediatamente invocar uma funcao
+
+function QualquerCoisa() {
+    console.log('Qualquer coisa');
+}
+
+//função anonima = sem nome
+(function() {
+    const nome = 'Tiago';
+    console.log(nome);
+})();
+
+const nome = 'Qualquer Coisa';
