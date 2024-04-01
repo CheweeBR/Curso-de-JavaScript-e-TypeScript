5ab5a308523558e2ee52efdf850f1582f60f4d64
Tiago Eloy Possidonio <105402460+CheweeBR@users.noreply.github.com>
2024-04-01 17:52:23 +0000

    Learning about Inheritance using Prototype.

--- Aulas/Javascript - Objetos (Avançado)/Aula-94.js
+++ Aulas/Javascript - Objetos (Avançado)/Aula-94.js
@@ -3,33 +3,32 @@
 // Todas funções construtoras possuem um protótipo no Javascript
 
 const objA = {
-    chave:'a'
-}
+  chave: "a",
+};
 const objB = {
-    chave:'b'
-}
+  chave: "b",
+};
 const objC = {
-    chave:'c'
-}
+  chave: "c",
+};
 
 Object.setPrototypeOf(objC, objB);
 Object.setPrototypeOf(objB, objA);
- 
-console.log(objC)
+
+console.log(objC);
 
 // ===================================
 
 function Produto(nome, valor) {
-    this.nome = nome;
-    this.valor = valor;
+  this.nome = nome;
+  this.valor = valor;
 }
 
-Produto.prototype.desconto = function(per) {
-    this.valor = this.valor - (this.valor * (per / 100));
-}
-    
+Produto.prototype.desconto = function (per) {
+  this.valor = this.valor - this.valor * (per / 100);
+};
 
-const produto1 = new Produto("Arroz", 2.40);
+const produto1 = new Produto("Arroz", 2.4);
 
-produto1.desconto(50)
+produto1.desconto(50);
 console.log(produto1.valor);
