0ccacdd8acd4a2c09bb8ab50233d109b3df1f5b6
Tiago Eloy Possidonio <105402460+CheweeBR@users.noreply.github.com>
2024-06-12 19:39:13 +0000

    Learning about async and await

---
+++ Aulas/JavaScript - Assíncrono/Aula110/aula.js
@@ -0,0 +1,30 @@
+// async e await
+function espera(msg, time) {
+    return new Promise((resolve, reject) => {
+
+        if(msg.length > 20) {
+            reject("MENSAGEM MUITO GRANDE.");
+            return;
+        }
+
+        setTimeout(() => {
+            resolve(msg);
+            return;
+        }, time);
+    })
+}
+
+async function executa() {
+    try {
+        const fase1 = await espera("Fase 1", 3000)
+        console.log(fase1);
+        const fase2 = await espera("Fase 2", 2000)
+        console.log(fase2);
+        const fase3 = await espera("Fase 3", 1000)
+        console.log(fase3);
+    } catch (e) {
+        console.log(e)
+    }
+}
+
+executa();
---
+++ Aulas/JavaScript - Assíncrono/Aula110/main.js
@@ -0,0 +1,45 @@
+const ajax = obj => {
+    const xhr = new XMLHttpRequest();
+    xhr.open(obj.method, obj.url, true);
+    xhr.send();
+
+    xhr.addEventListener('load', () => {
+        if(xhr.status >= 200 && xhr.status < 300) {
+            obj.success(xhr.responseText);  // Corrigido aqui
+        } else {
+            obj.error(xhr.status);
+        }
+    });
+};
+
+document.addEventListener('click', e => {
+    const el = e.target;
+    const tag = el.tagName.toLowerCase();
+
+    if(tag === 'a') {
+        e.preventDefault();
+        carregarPagina(el);
+    }
+});
+
+function carregarPagina(el) {
+    const href = el.getAttribute('href');  // Corrigido aqui
+
+    ajax({  // Corrigido aqui
+        method: 'GET',
+        url: href,
+        success(response) {  // Corrigido aqui
+            carregaResultado(response);
+        }, 
+        error(errorText) {
+            console.log(errorText);
+        }
+    });
+}
+
+function carregaResultado(response) {
+    const resultado = document.querySelector('.resultado');
+    resultado.innerHTML = response;
+}
+
+
